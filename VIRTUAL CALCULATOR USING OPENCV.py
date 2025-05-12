6d34cdd721f4d0b1ae217671a3bfaa448e3c4d04
unknown <aksayush2005@gmail.com>
2025-05-12 21:31:04 +0530

    Calaculator UI improved

--- VIRTUAL CALCULATOR USING OPENCV.py
+++ VIRTUAL CALCULATOR USING OPENCV.py
@@ -1,15 +1,25 @@
 import cv2
 import mediapipe as mp
-
+import time
 cap=cv2.VideoCapture(0)
 cap.set(3,1400)
 cap.set(4,800)
 def calculator_UI(frame,button_list):
+    frame_height,frame_width=frame.shape[:2]
     for button in button_list:
         x,y=button.pos
         w,h=button.size
         cv2.rectangle(frame,(100*y+25,100*x+25),(100*y+w+25,100*x+h+25),(0,255,255),2)
         cv2.putText(frame,str(button.value),(100*y+70,100*x+70),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),2)
+        if results.right_hand_landmarks:
+            index_finger_tip=results.right_hand_landmarks.landmark[8]
+            index_finger_tip_x=index_finger_tip.x
+            index_finger_tip_y=index_finger_tip.y
+            if 100*y+25<index_finger_tip_x*frame_width<100*y+w+25 and 100*x+25 <index_finger_tip_y*frame_height<100*x+h+25:
+                cv2.rectangle(frame,(100*y+25,100*x+25),(100*y+w+25,100*x+h+25),(0,0,255),2)
+                cv2.putText(frame, str(button.value), (100 * y + 70, 100 * x + 70), cv2.FONT_HERSHEY_PLAIN, 4,(0,0, 255), 2)
+
+
 class Buttons:
     def __init__(self,pos,value,size=(100,100)):
         self.pos=pos
