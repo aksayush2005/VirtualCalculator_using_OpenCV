2b34c402f8034c451fdff33d0f932310bbb20e8a
unknown <aksayush2005@gmail.com>
2025-05-12 17:43:52 +0530

    Basic calculator UI added

--- VIRTUAL CALCULATOR USING OPENCV.py
+++ VIRTUAL CALCULATOR USING OPENCV.py
@@ -2,8 +2,31 @@ import cv2
 import mediapipe as mp
 
 cap=cv2.VideoCapture(0)
-cap.set(3,500)
-cap.set(4,740)
+cap.set(3,1400)
+cap.set(4,800)
+def calculator_UI(frame,button_list):
+    for button in button_list:
+        x,y=button.pos
+        w,h=button.size
+        cv2.rectangle(frame,(100*y+25,100*x+25),(100*y+w+25,100*x+h+25),(0,255,255),2)
+        cv2.putText(frame,str(button.value),(100*y+70,100*x+70),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),2)
+class Buttons:
+    def __init__(self,pos,value,size=(100,100)):
+        self.pos=pos
+        self.value=value
+        self.size=size
+buttons=[[7,8,9,'+'],
+         [4,5,6,'-'],
+         [1,2,3,'/'],
+         ['=',0,"C",'*']]
+button_list=[]
+for i in range(0,len(buttons)):
+    for j,value in enumerate(buttons[i]):
+        button_list.append(Buttons((i,j),value))
+
+
+
+
 mp_drawing=mp.solutions.drawing_utils
 mp_holistic=mp.solutions.holistic
 with mp_holistic.Holistic(min_detection_confidence=0.7,min_tracking_confidence=0.7) as holistic:
@@ -23,7 +46,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.7,min_tracking_confidence=0
         right_list=results.right_hand_landmarks.landmark
         x=right_list[8].x
         cv2.putText(frame,f'{x}',(60,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-
+    calculator_UI(frame,button_list)
     cv2.imshow("Video",frame)
     if cv2.waitKey(1) & 0xFF==ord("q"):
        break
