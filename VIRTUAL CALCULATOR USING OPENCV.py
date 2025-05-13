fff90a6993f71a2ce3e6b20f3ab24406d03800a5
unknown <aksayush2005@gmail.com>
2025-05-13 21:40:42 +0530

    Calculator UI and functionality improved

--- VIRTUAL CALCULATOR USING OPENCV.py
+++ VIRTUAL CALCULATOR USING OPENCV.py
@@ -9,54 +9,59 @@ def calculator_UI(frame,button_list):
     for button in button_list:
         x,y=button.pos
         w,h=button.size
-        cv2.rectangle(frame,(100*y+25,100*x+25),(100*y+w+25,100*x+h+25),(0,255,255),2)
-        cv2.putText(frame,str(button.value),(100*y+70,100*x+70),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),2)
+        cv2.rectangle(frame,(100*y+65,100*x+65),(100*y+w+65,100*x+h+65),(0,255,255),-1)
+        cv2.putText(frame,str(button.value),(100*y+110,100*x+110),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),2)
         if results.right_hand_landmarks:
-            index_finger_tip=results.right_hand_landmarks.landmark[8]
-            index_finger_tip_x=index_finger_tip.x
-            index_finger_tip_y=index_finger_tip.y
-            thumb_tip=results.right_hand_landmarks.landmark[4]
-            thumb_tip_x=thumb_tip.x
-            thumb_tip_y=thumb_tip.y
-            distance = ((((index_finger_tip_x - thumb_tip_x) * frame_width) ** 2 + ((index_finger_tip_y - thumb_tip_y) * frame_height) ** 2) )**0.5
-            if 100*y+25<index_finger_tip_x*frame_width<100*y+w+25 and 100*x+25 <index_finger_tip_y*frame_height<100*x+h+25:
-                cv2.rectangle(frame,(100*y+25,100*x+25),(100*y+w+25,100*x+h+25),(0,0,255),2)
-                cv2.putText(frame, str(button.value), (100 * y + 70, 100 * x + 70), cv2.FONT_HERSHEY_PLAIN, 4,(0,0, 255), 2)
-                if distance<56:
-                    cv2.rectangle(frame, (100 * y + 25, 100 * x + 25), (100 * y + w + 25, 100 * x + h + 25),(255, 255, 0), 2)
+            right_index_tip=results.right_hand_landmarks.landmark[8]
+            right_index_tip_x=right_index_tip.x
+            right_index_tip_y=right_index_tip.y
+            right_thumb_tip=results.right_hand_landmarks.landmark[4]
+            right_thumb_tip_x=right_thumb_tip.x
+            right_thumb_tip_y=right_thumb_tip.y
+            distance_right= ((((right_index_tip_x - right_thumb_tip_x) * frame_width) ** 2 + ((right_index_tip_y - right_thumb_tip_y) * frame_height) ** 2) )**0.5
+            if 100*y+65<right_index_tip_x*frame_width<100*y+w+65 and 100*x+65 <right_index_tip_y*frame_height<100*x+h+65:
+                cv2.rectangle(frame,(100*y+65,100*x+65),(100*y+w+65,100*x+h+65),(0,0,255),-1)
+                cv2.putText(frame, str(button.value), (100 * y + 110, 100 * x + 110), cv2.FONT_HERSHEY_PLAIN, 4,(0,0, 255), 2)
+                if distance_right<55:
+                    cv2.rectangle(frame, (100 * y + 65, 100 * x + 65), (100 * y + w + 65, 100 * x + h + 65),(255, 255, 0), -1)
                     #cv2.putText(frame,str(distance),(60,60),cv2.FONT_HERSHEY_PLAIN,4,(255,255,0),2)
-                    cv2.putText(frame, str(button.value), (100 * y + 70, 100 * x + 70), cv2.FONT_HERSHEY_PLAIN, 4,(255,255,0), 2)
-                    time.sleep(0.25)
+                    cv2.putText(frame, str(button.value), (100 * y + 110, 100 * x + 110), cv2.FONT_HERSHEY_PLAIN, 4,(255,255,0), 2)
+                    time.sleep(0.15)
                     result=calculator(button.value)
-                    cv2.putText(frame,str(result),(160,160),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),2)
+                    cv2.putText(frame,str(result),(700,160),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),2)
+
 
 exp_list=[]
 def calculator(value):
     exp=''
-
-    if value != '=' and value != 'C':
+    try:
+        if value != '=' and value != 'C':
             exp_list.append(value)
-            return " ".join(str(i) for i  in exp_list)
 
-    else:
+            return "".join(str(i) for i  in exp_list)
 
-        if value == '=':
+        else:
+
+            if value == '=':
                 for i in exp_list:
                     exp=exp+str(i)
 
                 exp=eval(exp)
+
                 return exp
-        elif value == 'C':
+            elif value == 'C':
                 exp_list.clear()
                 return ""
-
+    except:
         return "Invalid input"
 
 
 
 
+
+
 class Buttons:
-    def __init__(self,pos,value,size=(100,100)):
+    def __init__(self,pos,value,size=(90,90)):
         self.pos=pos
         self.value=value
         self.size=size
@@ -75,7 +80,9 @@ for i in range(0,len(buttons)):
 mp_drawing=mp.solutions.drawing_utils
 mp_holistic=mp.solutions.holistic
 with mp_holistic.Holistic(min_detection_confidence=0.7,min_tracking_confidence=0.7) as holistic:
+
   while cap.isOpened():
+
     ret,frame=cap.read()
     frame = cv2.flip(frame, 1)
     image=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
@@ -92,6 +99,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.7,min_tracking_confidence=0
         x=right_list[8].x
         cv2.putText(frame,f'{x}',(60,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)'''
     calculator_UI(frame,button_list)
+
     cv2.imshow("Video",frame)
     if cv2.waitKey(1) & 0xFF==ord("q"):
        break
