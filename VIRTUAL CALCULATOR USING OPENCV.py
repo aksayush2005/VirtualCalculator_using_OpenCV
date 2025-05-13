3b80698cd30e6bdf32480ba45d80d6f9c3accd4f
unknown <aksayush2005@gmail.com>
2025-05-13 09:51:23 +0530

    Touch added

--- VIRTUAL CALCULATOR USING OPENCV.py
+++ VIRTUAL CALCULATOR USING OPENCV.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
 cap=cv2.VideoCapture(0)
 cap.set(3,1400)
 cap.set(4,800)
@@ -15,11 +14,17 @@ def calculator_UI(frame,button_list):
             index_finger_tip=results.right_hand_landmarks.landmark[8]
             index_finger_tip_x=index_finger_tip.x
             index_finger_tip_y=index_finger_tip.y
+            thumb_tip=results.right_hand_landmarks.landmark[4]
+            thumb_tip_x=thumb_tip.x
+            thumb_tip_y=thumb_tip.y
+            distance = ((((index_finger_tip_x - thumb_tip_x) * frame_width) ** 2 + ((index_finger_tip_y - thumb_tip_y) * frame_height) ** 2) )**0.5
             if 100*y+25<index_finger_tip_x*frame_width<100*y+w+25 and 100*x+25 <index_finger_tip_y*frame_height<100*x+h+25:
                 cv2.rectangle(frame,(100*y+25,100*x+25),(100*y+w+25,100*x+h+25),(0,0,255),2)
                 cv2.putText(frame, str(button.value), (100 * y + 70, 100 * x + 70), cv2.FONT_HERSHEY_PLAIN, 4,(0,0, 255), 2)
-
-
+                if distance<56:
+                    cv2.rectangle(frame, (100 * y + 25, 100 * x + 25), (100 * y + w + 25, 100 * x + h + 25),(255, 255, 0), 2)
+                    cv2.putText(frame,str(distance),(60,60),cv2.FONT_HERSHEY_PLAIN,4,(255,255,0),2)
+                    cv2.putText(frame, str(button.value), (100 * y + 70, 100 * x + 70), cv2.FONT_HERSHEY_PLAIN, 4,(255,255,0), 2)
 class Buttons:
     def __init__(self,pos,value,size=(100,100)):
         self.pos=pos
@@ -52,10 +57,10 @@ with mp_holistic.Holistic(min_detection_confidence=0.7,min_tracking_confidence=0
     mp_drawing.draw_landmarks(frame,results.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(frame,results.pose_landmarks,mp_holistic.POSE_CONNECTIONS)
-    if results.right_hand_landmarks:
+    '''if results.right_hand_landmarks:
         right_list=results.right_hand_landmarks.landmark
         x=right_list[8].x
-        cv2.putText(frame,f'{x}',(60,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        cv2.putText(frame,f'{x}',(60,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)'''
     calculator_UI(frame,button_list)
     cv2.imshow("Video",frame)
     if cv2.waitKey(1) & 0xFF==ord("q"):
