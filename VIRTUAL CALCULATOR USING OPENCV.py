ad68be2fd4450ea1ec3627654b6b87a59381d5a7
unknown <aksayush2005@gmail.com>
2025-05-30 11:08:34 +0530

    Air writing feature added

--- VIRTUAL CALCULATOR USING OPENCV.py
+++ VIRTUAL CALCULATOR USING OPENCV.py
@@ -3,14 +3,21 @@ import mediapipe as mp
 import time
 import pyttsx3
 import threading
+import numpy as np
 import copy
 engine = pyttsx3.init()
 cap=cv2.VideoCapture(0)
 cap.set(3,1400)
 cap.set(4,800)
+x0,y0=0,0
+mask=np.zeros((720,1280,3),np.uint8)
+flag=0
 def calculator_UI(frame,button_list):
+    cv2.line(frame,(700,0),(700,700),(255,255,255),2)
     frame_height,frame_width=frame.shape[:2]
     global result
+    global x0,y0
+    global flag
     for button in button_list:
         x,y=button.pos
         w,h=button.size
@@ -30,12 +37,43 @@ def calculator_UI(frame,button_list):
                 cv2.putText(frame, str(button.value), (100 * y + 110, 100 * x + 110), cv2.FONT_HERSHEY_PLAIN, 4,(0,0,0), 2)
                 time.sleep(0.25)
                 if distance_right<50:
-                    cv2.rectangle(frame, (100 * y + 65, 100 * x + 65), (100 * y + w + 65, 100 * x + h + 65),(114,88, 2), -1)
-                    #cv2.putText(frame,str(distance),(60,60),cv2.FONT_HERSHEY_PLAIN,4,(255,255,0),2)
-                    cv2.putText(frame, str(button.value), (100 * y + 110, 100 * x + 110), cv2.FONT_HERSHEY_PLAIN, 4,(255,255,255), 2)
+                        cv2.rectangle(frame, (100 * y + 65, 100 * x + 65), (100 * y + w + 65, 100 * x + h + 65),(114,88, 2), -1)
+                        #cv2.putText(frame,str(distance),(60,60),cv2.FONT_HERSHEY_PLAIN,4,(255,255,0),2)
+                        cv2.putText(frame, str(button.value), (100 * y + 110, 100 * x + 110), cv2.FONT_HERSHEY_PLAIN, 4,(255,255,255), 2)
+                        #cv2.line(frame,(right_thumb_tip_x*frame_width,right_thumb_tip_y*frame_height),(right_thumb_tip_x*frame_width,right_thumb_tip_y*frame_height),(255,0,255),2)
+                        result=calculator(button.value)
 
-                    result=calculator(button.value)
-        cv2.putText(frame,str(result),(700,160),cv2.FONT_HERSHEY_PLAIN,4,(0,0,0),4)
+
+            if 700<right_index_tip_x*frame_width<frame_width and 0<right_index_tip_y*frame_height<700:
+
+                right_index_tip=results.right_hand_landmarks.landmark[8]
+                right_index_tip_x=right_index_tip.x
+                right_index_tip_y=right_index_tip.y
+                right_thumb_tip=results.right_hand_landmarks.landmark[4]
+                right_thumb_tip_x=right_thumb_tip.x
+                right_thumb_tip_y=right_thumb_tip.y
+                distance_right= ((((right_index_tip_x - right_thumb_tip_x) * frame_width) ** 2 + ((right_index_tip_y - right_thumb_tip_y) * frame_height) ** 2) )**0.5
+                if distance_right<25:
+                    if flag==1:
+                        x0,y0=0,0
+                        flag=0
+
+                    xp, yp = (right_index_tip.x) * frame_width, (right_index_tip.y) * frame_height
+                    if (x0,y0)==(0,0):
+                        x0,y0=xp,yp
+
+                    cv2.line(mask,(int(x0),int(y0)),(int(xp),int(yp)),(255,255,255),2)
+                    x0,y0=xp,yp
+
+
+
+
+                    #print(xp,yp)
+
+                    #for i in drawing_list:
+                        #cv2.circle(frame,i,100,(255,255,255),2)
+        cv2.putText(frame, str(result), (200,620), cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+
 
 
 
@@ -45,9 +83,11 @@ def speak(text):
 result= ''
 exp_list=[]
 def calculator(value):
+    global mask
     exp=''
+    global flag
     try:
-        if value != '=' and value != 'C':
+        if value != '=' and value != 'C' and value != 'CAP' and value != 'E':
             exp_list.append(value)
             if value=='-':
                 threading.Thread(target=speak,args=("minus",)).start()
@@ -55,10 +95,17 @@ def calculator(value):
                 threading.Thread(target=speak, args=("multiplied by",)).start()
             elif value=="/":
                 threading.Thread(target=speak, args=("divided by",)).start()
+
             else:
                 threading.Thread(target=speak, args=(value,)).start()
 
             return "".join(str(i) for i  in exp_list)
+        elif value=='E' or value=='CAP':
+            if value == 'E':
+                flag=1
+                mask=np.zeros((720,1280,3),np.uint8)
+                return "".join(str(i) for i  in exp_list)
+
 
         else:
 
@@ -93,7 +140,8 @@ class Buttons:
 buttons=[[7,8,9,'+'],
          [4,5,6,'-'],
          [1,2,3,'/'],
-         ['=',0,"C",'*']]
+         ['=',0,"C",'*'],
+         ['P','','','E']]
 button_list=[]
 for i in range(0,len(buttons)):
     for j,value in enumerate(buttons[i]):
@@ -110,21 +158,23 @@ with mp_holistic.Holistic(min_detection_confidence=0.7,min_tracking_confidence=0
 
     ret,frame=cap.read()
     frame = cv2.flip(frame, 1)
+
     image=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     results=holistic.process(image)
     image=cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
-    #mp_drawing.draw_landmarks(frame,results.face_landmarks,mp_holistic.FACEMESH_TESSELATION,
-                              #mp_drawing.DrawingSpec(color=[255,0,0],thickness=1,circle_radius=1),
-                              #mp_drawing.DrawingSpec(color=[255,0,0],thickness=1,circle_radius=1))
-    mp_drawing.draw_landmarks(frame,results.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
-    mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(frame,results.face_landmarks,mp_holistic.FACEMESH_TESSELATION,
+                              mp_drawing.DrawingSpec(color=[255,0,0],thickness=1,circle_radius=1),
+                              mp_drawing.DrawingSpec(color=[255,0,0],thickness=1,circle_radius=1))
+    #mp_drawing.draw_landmarks(frame,results.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
+    #mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     #mp_drawing.draw_landmarks(frame,results.pose_landmarks,mp_holistic.POSE_CONNECTIONS)
     '''if results.right_hand_landmarks:
         right_list=results.right_hand_landmarks.landmark
         x=right_list[8].x
         cv2.putText(frame,f'{x}',(60,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)'''
     calculator_UI(frame,button_list)
-
+    frame=cv2.addWeighted(mask,1,frame,1,0)
+    cv2.imshow('drawingboard',mask)
     cv2.imshow("Video",frame)
     if cv2.waitKey(1) & 0xFF==ord("q"):
        break
